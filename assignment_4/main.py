4ed070b3ad12b400ca157199d0950aa03cdeb043
tomvig <tom.vig@mail.huji.ac.il>
2025-01-05 16:50:33 +0100

    started first experiment

--- assignment_4/main.py
+++ assignment_4/main.py
@@ -16,13 +16,14 @@ def create_codebook(training_images, k_means):
     for im in training_images:
         data = im.get_data()
         _, des = sift.detectAndCompute(data, None)
-        descriptors.append(des)        
+        descriptors.append(des)
         im.set_descriptors(des)
     matrix = np.vstack(descriptors)
     criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 10, 1.0)
     flags = cv.KMEANS_RANDOM_CENTERS
     compactness, labels, codebook = cv.kmeans(matrix, k_means, None, criteria, 10, flags)
     return compactness, labels, codebook
+
 
 def read_dataset(dataset_folder, images_per_category, selected_categories):
     training_images = []
@@ -37,7 +38,8 @@ def read_dataset(dataset_folder, images_per_category, selected_categories):
                 test_images.append(Image(image, category))
     return training_images, test_images
 
-def run_elbow_test():
+
+def run_elbow_test(training_images):
     k_means_values = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600]
     compactness_values = []
     for k in k_means_values:
@@ -47,6 +49,7 @@ def run_elbow_test():
 
     plt.plot(k_means_values, compactness_values)
     plt.show()
+
 
 # The retreival function
 #
@@ -60,27 +63,68 @@ def run_elbow_test():
 
 def retreive(dataset, query_hist):
     query_hist = np.array(query_hist)
-    
+
     # Calculate 'common words' similarity
     def common_words_similarity(hist):
         return np.sum(np.minimum(query_hist, np.array(hist)))
-    
+
+    # Create a copy of the dataset to avoid SettingWithCopyWarning
+    dataset_copy = dataset.copy()
+
     # Apply similarity calculation to each row in the dataset
-    dataset['similarity'] = dataset['hist'].apply(common_words_similarity)
-    
+    dataset_copy.loc[:, 'similarity'] = dataset_copy['hist'].apply(common_words_similarity)
+
     # Sort by similarity in descending order
-    ranked_dataset = dataset.sort_values(by='similarity', ascending=False)
-    
+    ranked_dataset = dataset_copy.sort_values(by='similarity', ascending=False)
+
     return ranked_dataset
+
+
+def experiment_1(training_data):
+    reciprocal_ranks = []
+    correct_in_top3 = 0
+    total_queries = len(training_data)
+
+    for idx, query_row in training_data.iterrows():
+        query_hist = query_row['hist']
+        true_label = query_row['true_label']
+
+        # Perform retrieval
+        ranked_dataset = retreive(training_data, query_hist)
+
+        # Calculate rank of the first correct match
+        rank = 1
+        for _, row in ranked_dataset.iterrows():
+            if row['true_label'] == true_label:
+                reciprocal_ranks.append(1 / rank)
+                if rank <= 3:
+                    correct_in_top3 += 1
+                break
+            rank += 1
+
+    # Calculate MRR
+    mrr = sum(reciprocal_ranks) / total_queries
+
+    # Calculate Top-3 Accuracy
+    top3_accuracy = (correct_in_top3 / total_queries) * 100
+
+    # Print final metrics
+    print("\nExperiment 1 Summary:")
+    print(f"Total Queries: {total_queries}")
+    print(f"Mean Reciprocal Rank (MRR): {mrr:.4f}")
+    print(f"Top-3 Accuracy: {top3_accuracy:.2f}%")
+
+    return mrr, top3_accuracy
+
 
 def main():
     data_file_name = 'data'
     dataset_folder = "101_ObjectCategories"
     images_per_category = 20
-    selected_categories = ["brain","cannon","ant","octopus","butterfly"]
+    selected_categories = ["brain", "cannon", "ant", "octopus", "butterfly"]
     training_images, test_images = read_dataset(dataset_folder, images_per_category, selected_categories)
 
-    #run_elbow_test()
+    # run_elbow_test(training_images)
 
     k = 300
     # Boolean to use file or generate new file, Change to false when updating any parameters above.
@@ -104,19 +148,19 @@ def main():
             data.loc[len(data)] = [im.get_path(), im.get_category(), True, im.get_histogram(codebook, k)]
         for im in test_images:
             data.loc[len(data)] = [im.get_path(), im.get_category(), False, im.get_histogram(codebook, k)]
-            
+
         # Save indexing to file
         data.to_csv('{}.csv'.format(data_file_name), index=False)
 
     # Get some trainig image.
-    query_train_image = data[data['is_train'] == True].iloc[0]
-    print(data.dtypes)
-    print("Running Retreival on {}".format(query_train_image))
-    ranking = retreive(data, query_train_image['hist'])
-    print(ranking.head(5))
+    # query_train_image = data[data['is_train'] == True].iloc[0]
+    # print(data.dtypes)
+    # print("Running Retreival on {}".format(query_train_image))
+    # ranking = retreive(data, query_train_image['hist'])
+    # print(ranking.head(5))
+
+    experiment_1(data[data['is_train'] == True])
 
 
-    
-        
 if __name__ == "__main__":
     main()
