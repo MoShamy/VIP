e0dec5b47ea6d866ca3432352af2c0b6f074ac4a
MostafaElshamy <mostafaelshamy@aucegypt.edu>
2025-01-08 17:47:44 +0100

    k-means not working yet

--- assignment_5/main.py
+++ assignment_5/main.py
@@ -6,64 +6,62 @@ Authors: Mostafa Elshamy, Tom Vig, Andreas Hammer
 import numpy as np
 from PIL import Image
 
-def k_means_clustering(image,name):
+def k_means_clustering(image_array,name,k=2):
     """
     K-means clustering
     1. Take grey image as input
     2. Run k-means on k=2
-    3. Run k-means for higher k values thresholf with lloyds Algorithm
+
+    """
+
+    centroids = get_rand_centroids(image_array,k)
+
+    image_array_label = get_image_array_label(image_array, centroids)
+
+    print(image_array_label)
+    save_image(image_array_label,name+"_k" + str(k))
+
+    
+    
+def lloyds_algorithm(image_array,name,k=3):
+    """
+        3. Run k-means for higher k values thresholf with lloyds Algorithm
         Start with k candidate centroids. How to choose them?
         Repeat:
             1. Label pixels / assign them to clusters from their feature distance to centroids.
             2. Recompute centroids of features as average of clusters until regions or centroids don’t change.
     """
-    
-    # 1.
-    image_array = np.array(image)
-    
-    
-    # 2. Run k-means on k=2
-    k = 2
-    # get centroids
     centroids = get_rand_centroids(image_array,k)
-
-    #repeat until centroids don't change
-    while True:
+    j=0
+    while j<100:
 
         # Assign each pixel to the nearest centroid
         image_array_label = get_image_array_label(image_array, centroids)
 
-
-        # 3. Run k-means for higher k values threshold with lloyds Algorithm
         # recompute centroids based on prev run. average of pixels in each label
-
+        new_centroids = []
         for label in range(k):
             cluster = image_array[image_array_label == label]
-            centroids[label] = np.mean(cluster, axis=0)
-        # Assign each pixel to the nearest centroid
-        image_array_label = assign_pixels_to_clusters(image_array, centroids)
+            new_centroid = np.mean(cluster, axis=0)
+            new_centroids.append(new_centroid)
 
-        # recompute centroids based on prev run. average of pixels in each label
-        for label in range(k):
-            cluster = image_array[image_array_label == label]
-            new_centroid = int(np.mean(cluster, axis=0))
-            
-            if np.array_equal(new_centroid, centroids[label]):
-                save_image(image_array,image_array_label,name)
-                break
-            else:
-                centroids[label] = new_centroid
-            continue
-    
+        if np.allclose(new_centroids, centroids,atol=10): #check tolerance
+            save_image(image_array_label,name+"_Lloyds_k" + str(k))
+            return image_array_label,centroids
+        else:
+            centroids = new_centroids
+            j+=1
+        continue
+    print("Lloyds Algorithm did not converge")
 
-def get_image_array_label(image_array, centroids):
+def get_image_array_label(image_array, centroids): #done
     image_array_label = np.zeros(image_array.shape)
     for x in range(image_array.shape[0]):
         for y in range(image_array.shape[1]):
             image_array_label[x,y] = get_nearest_centroid(enumerate(centroids), image_array[x][y])
     return image_array_label
 
-def assign_pixels_to_clusters(image_array, centroids):
+def assign_pixels_to_clusters(image_array, centroids): #done
     """
     Assign each pixel to the nearest centroid
     tool for k-means clustering
@@ -75,7 +73,7 @@ def assign_pixels_to_clusters(image_array, centroids):
             image_array_label[x,y] = nearest_centroid
     return image_array_label
 
-def get_rand_centroids(image_array,k):
+def get_rand_centroids(image_array,k): #done
     centroids = []
     for i in range(k):
         centroids.append( [(np.random.randint(0, image_array.max()) ) , (np.random.randint(0, image_array[1].max())) ] )
@@ -89,26 +87,23 @@ def save_image(image_array_label ,name):
     Tool for clustering
     """
     # Increase contrast between labels
-    # unique_labels = np.unique(image_array_label)
-    # label_map = {label: i * (255 // (len(unique_labels) - 1)) for i, label in enumerate(unique_labels)}
-    # high_contrast_image_array = np.vectorize(label_map.get)(image_array_label)
+    image_label = Image.fromarray(image_array_label)
+    image_label = image_label.point(lambda i: i * 255)
+    image_label = image_label.convert("L")
 
-    # Convert to image, showing the labels in different colors
-    image_label = Image.fromarray(image_array_label.astype(np.uint8))
-
-    image_label.save("Results/"+name +"_2k.png")
+    image_label.save("Results/"+name +".png")
 
 
     
-def get_nearest_centroid(image_array,centroids, pixel):
+def get_nearest_centroid(centroids_values, pixel_value): #done
     """
     Get the nearest centroid to a pixel
     tool for k-means clustering
     """
     min_distance = np.inf
     nearest_centroid = None
-    for i, centroid in centroids:
-        distance = np.linalg.norm(np.array(image_array[centroid]) - np.array(image_array[pixel]))
+    for i, centroid in centroids_values:
+        distance = centroid - pixel_value
         if distance < min_distance:
             min_distance = distance
             nearest_centroid = i
@@ -126,7 +121,10 @@ def main():
     image = Image.open("images/"+ image_name +".png")
     #  Grey-scale the image
     image_grey = image.convert("L")
-    k_means_clustering(image_grey,image_name)
+    image_array = np.array(image_grey)
+    k_means_clustering(image_array,image_name,15)
+    # k_means_clustering(image_array,image_name,3)
+    # lloyds_algorithm(image_array,image_name,3)
 
 
 
